20f332e4d363f17f0bbb706651b91f42d18b11af
sasiloxr <sasiloxr2@gmail.com>
2025-05-30 20:23:58 +0800

    upload github

--- src/main/java/keystrokesmod/module/impl/client/Settings.java
+++ src/main/java/keystrokesmod/module/impl/client/Settings.java
@@ -1,5 +1,12 @@
 package keystrokesmod.module.impl.client;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
 import keystrokesmod.Raven;
 import keystrokesmod.module.Module;
 import keystrokesmod.module.setting.impl.ButtonSetting;
@@ -10,112 +17,111 @@ import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.util.ResourceLocation;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Settings extends Module {
-    public static SliderSetting customCapes;
-    public static ButtonSetting weaponAxe;
-    public static ButtonSetting weaponRod;
-    public static ButtonSetting weaponStick;
-    public static ButtonSetting middleClickFriends;
-    public static ButtonSetting setChatAsInventory;
+  public static SliderSetting customCapes;
+  public static ButtonSetting weaponAxe;
+  public static ButtonSetting weaponRod;
+  public static ButtonSetting weaponStick;
+  public static ButtonSetting middleClickFriends;
+  public static ButtonSetting setChatAsInventory;
 
-    public static ButtonSetting rotateBody;
-    public static ButtonSetting fullBody;
-    public static ButtonSetting movementFix;
-    public static ButtonSetting strictMove;
-    public static SliderSetting randomYawFactor;
+  public static ButtonSetting rotateBody;
+  public static ButtonSetting fullBody;
+  public static ButtonSetting movementFix;
+  public static ButtonSetting strictMove;
+  public static SliderSetting randomYawFactor;
 
-    public static ButtonSetting loadGuiPositions;
-    public static ButtonSetting sendMessage;
+  public static ButtonSetting loadGuiPositions;
+  public static ButtonSetting sendMessage;
 
-    public static SliderSetting offset;
-    public static SliderSetting timeMultiplier;
+  public static SliderSetting offset;
+  public static SliderSetting timeMultiplier;
 
-    private String[] capes = new String[]{"None", "Anime", "Aqua", "Green", "Purple", "Red", "White", "Yellow", "Custom"};
-    public File directory;
+  private String[] capes =
+      new String[] {"None", "Anime", "Aqua", "Green", "Purple", "Red", "White", "Yellow", "Custom"};
+  public File directory;
 
-    public static List<ResourceLocation> loadedCapes = new ArrayList<>();
+  public static List<ResourceLocation> loadedCapes = new ArrayList<>();
 
-    public Settings() {
-        super("Settings", category.client, 0);
-        this.registerSetting(new DescriptionSetting("General"));
-        this.registerSetting(customCapes = new SliderSetting("Custom cape", 0, capes));
-        this.registerSetting(weaponAxe = new ButtonSetting("Set axe as weapon", false));
-        this.registerSetting(weaponRod = new ButtonSetting("Set rod as weapon", false));
-        this.registerSetting(weaponStick = new ButtonSetting("Set stick as weapon", false));
-        this.registerSetting(middleClickFriends = new ButtonSetting("Middle click friends", false));
-        this.registerSetting(setChatAsInventory = new ButtonSetting("Set chat as inventory", false));
-        this.registerSetting(new DescriptionSetting("Rotations"));
-        this.registerSetting(rotateBody = new ButtonSetting("Rotate body", true));
-        this.registerSetting(fullBody = new ButtonSetting("Full body", false));
-        this.registerSetting(movementFix = new ButtonSetting("Movement fix", false));
-        this.registerSetting(strictMove = new ButtonSetting("StrictMove", false));
-        this.registerSetting(randomYawFactor = new SliderSetting("Random yaw factor", 1.0, 0.0, 10.0, 1.0));
-        this.registerSetting(new DescriptionSetting("Profiles"));
-        this.registerSetting(loadGuiPositions = new ButtonSetting("Load gui state", false));
-        this.registerSetting(sendMessage = new ButtonSetting("Send message on enable", true));
-        this.registerSetting(new DescriptionSetting("Theme colors"));
-        this.registerSetting(offset = new SliderSetting("Offset", 0.5, -3.0, 3.0, 0.1));
-        this.registerSetting(timeMultiplier = new SliderSetting("Time multiplier", 0.5, 0.1, 4.0, 0.1));
-        this.canBeEnabled = false;
-        directory = new File(mc.mcDataDir + File.separator + "capes");
-        if (!directory.exists()) {
-            boolean success = directory.mkdirs();
-            if (!success) {
-                System.out.println("There was an issue creating capes directory.");
-                return;
-            }
-        }
-        if (directory.listFiles().length == 0) {
-            System.out.println("None Load Custom Cape");
-        }
-        loadCapes(directory.listFiles());
+  public Settings() {
+    super("Settings", category.client, 0);
+    this.registerSetting(new DescriptionSetting("General"));
+    this.registerSetting(customCapes = new SliderSetting("Custom cape", 0, capes));
+    this.registerSetting(weaponAxe = new ButtonSetting("Set axe as weapon", false));
+    this.registerSetting(weaponRod = new ButtonSetting("Set rod as weapon", false));
+    this.registerSetting(weaponStick = new ButtonSetting("Set stick as weapon", false));
+    this.registerSetting(middleClickFriends = new ButtonSetting("Middle click friends", false));
+    this.registerSetting(setChatAsInventory = new ButtonSetting("Set chat as inventory", false));
+    this.registerSetting(new DescriptionSetting("Rotations"));
+    this.registerSetting(rotateBody = new ButtonSetting("Rotate body", true));
+    this.registerSetting(fullBody = new ButtonSetting("Full body", false));
+    this.registerSetting(movementFix = new ButtonSetting("Movement fix", false));
+    this.registerSetting(strictMove = new ButtonSetting("StrictMove", true));
+    this.registerSetting(
+        randomYawFactor = new SliderSetting("Random yaw factor", 1.0, 0.0, 10.0, 1.0));
+    this.registerSetting(new DescriptionSetting("Profiles"));
+    this.registerSetting(loadGuiPositions = new ButtonSetting("Load gui state", false));
+    this.registerSetting(sendMessage = new ButtonSetting("Send message on enable", true));
+    this.registerSetting(new DescriptionSetting("Theme colors"));
+    this.registerSetting(offset = new SliderSetting("Offset", 0.5, -3.0, 3.0, 0.1));
+    this.registerSetting(timeMultiplier = new SliderSetting("Time multiplier", 0.5, 0.1, 4.0, 0.1));
+    this.canBeEnabled = false;
+    directory = new File(mc.mcDataDir + File.separator + "capes");
+    if (!directory.exists()) {
+      boolean success = directory.mkdirs();
+      if (!success) {
+        System.out.println("There was an issue creating capes directory.");
+        return;
+      }
     }
-
-    public void loadCapes(File[] CustomCapes) {
-        try {
-            for (int i = 1; i < capes.length; i++) {
-                if (capes[i].equals("Custom")) {
-                    System.out.println("Load Custom Cape");
-                    if (CustomCapes.length == 0) {
-                        loadedCapes.add(null);
-                        continue;
-                    }
-                    InputStream stream = Files.newInputStream(CustomCapes[0].toPath());
-                    BufferedImage bufferedImage = ImageIO.read(stream);
-                    loadedCapes.add(mc.renderEngine.getDynamicTextureLocation("Custom", new DynamicTexture(bufferedImage)));
-                } else {
-                    String name = capes[i].toLowerCase();
-                    if (i > 1) {
-                        name = "rvn_" + name;
-                    }
-                    InputStream stream = Raven.class.getResourceAsStream("/assets/keystrokesmod/textures/capes/" + name + ".png");
-                    if (stream == null) {
-                        continue;
-                    }
-                    BufferedImage bufferedImage = ImageIO.read(stream);
-                    loadedCapes.add(mc.renderEngine.getDynamicTextureLocation(name, new DynamicTexture(bufferedImage)));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    if (directory.listFiles().length == 0) {
+      System.out.println("None Load Custom Cape");
     }
+    loadCapes(directory.listFiles());
+  }
 
-    public static boolean inInventory() {
-        if (mc.currentScreen instanceof GuiInventory) {
-            return true;
+  public void loadCapes(File[] CustomCapes) {
+    try {
+      for (int i = 1; i < capes.length; i++) {
+        if (capes[i].equals("Custom")) {
+          System.out.println("Load Custom Cape");
+          if (CustomCapes.length == 0) {
+            loadedCapes.add(null);
+            continue;
+          }
+          InputStream stream = Files.newInputStream(CustomCapes[0].toPath());
+          BufferedImage bufferedImage = ImageIO.read(stream);
+          loadedCapes.add(
+              mc.renderEngine.getDynamicTextureLocation(
+                  "Custom", new DynamicTexture(bufferedImage)));
+        } else {
+          String name = capes[i].toLowerCase();
+          if (i > 1) {
+            name = "rvn_" + name;
+          }
+          InputStream stream =
+              Raven.class.getResourceAsStream(
+                  "/assets/keystrokesmod/textures/capes/" + name + ".png");
+          if (stream == null) {
+            continue;
+          }
+          BufferedImage bufferedImage = ImageIO.read(stream);
+          loadedCapes.add(
+              mc.renderEngine.getDynamicTextureLocation(name, new DynamicTexture(bufferedImage)));
         }
-        if (mc.currentScreen instanceof GuiChat && setChatAsInventory.isToggled()) {
-            return true;
-        }
-        return false;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
+
+  public static boolean inInventory() {
+    if (mc.currentScreen instanceof GuiInventory) {
+      return true;
+    }
+    if (mc.currentScreen instanceof GuiChat && setChatAsInventory.isToggled()) {
+      return true;
+    }
+    return false;
+  }
 }
--- src/main/java/keystrokesmod/module/impl/combat/AimAssist.java
+++ src/main/java/keystrokesmod/module/impl/combat/AimAssist.java
@@ -1,5 +1,7 @@
 package keystrokesmod.module.impl.combat;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import keystrokesmod.Raven;
 import keystrokesmod.event.ClientLookEvent;
 import keystrokesmod.event.RotationEvent;
@@ -18,201 +20,209 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
 public class AimAssist extends Module {
-    private SliderSetting mode;
-    private SliderSetting speed;
-    private SliderSetting fov;
-    private SliderSetting distance;
-    private ButtonSetting clickAim;
-    private ButtonSetting weaponOnly;
-    private ButtonSetting aimInvis;
-    private ButtonSetting blatantMode;
-    private ButtonSetting ignoreTeammates;
-    private ButtonSetting rangeFirst;
-    private ButtonSetting fovPriority;
-    private ButtonSetting aimNecessary;
-    public ButtonSetting hitThroughBlock;
-    public static EntityLivingBase targetEntity;
-    private static boolean shouldRender;
-    private static Float[] lookAt = null;
+  private SliderSetting mode;
+  private SliderSetting speed;
+  private SliderSetting fov;
+  private SliderSetting distance;
+  private ButtonSetting clickAim;
+  private ButtonSetting weaponOnly;
+  private ButtonSetting aimInvis;
+  private ButtonSetting blatantMode;
+  private ButtonSetting ignoreTeammates;
+  private ButtonSetting rangeFirst;
+  private ButtonSetting fovPriority;
+  private ButtonSetting aimNecessary;
+  public ButtonSetting hitThroughBlock;
+  public static EntityLivingBase targetEntity;
+  private static boolean shouldRender;
+  private static Float[] lookAt = null;
 
-    private String[] modes = new String[]{"Vanilla", "Silent"};
+  private String[] modes = new String[] {"Vanilla", "Silent"};
 
-    public AimAssist() {
-        super("AimAssist", category.combat, 0);
-        this.registerSetting(mode = new SliderSetting("Mode", 0, modes));
-        this.registerSetting(speed = new SliderSetting("Speed", 45.0D, 1.0D, 100.0D, 1.0D));
-        this.registerSetting(fov = new SliderSetting("FOV", 90.0D, 15.0D, 180.0D, 1.0D));
-        this.registerSetting(distance = new SliderSetting("Distance", 4.5D, 1.0D, 10.0D, 0.5D));
-        this.registerSetting(clickAim = new ButtonSetting("Click aim", true));
-        this.registerSetting(weaponOnly = new ButtonSetting("Weapon only", false));
-        this.registerSetting(aimInvis = new ButtonSetting("Aim invis", false));
-        this.registerSetting(blatantMode = new ButtonSetting("Blatant mode", false));
-        this.registerSetting(ignoreTeammates = new ButtonSetting("Ignore teammates", false));
-        this.registerSetting(rangeFirst = new ButtonSetting("Range First", false));
-        this.registerSetting(fovPriority = new ButtonSetting("Fov Priority", false));
-        this.registerSetting(aimNecessary = new ButtonSetting("Aim Necessary", false));
-        this.registerSetting(hitThroughBlock = new ButtonSetting("Hit Through Block", false));
+  public AimAssist() {
+    super("AimAssist", category.combat, 0);
+    this.registerSetting(mode = new SliderSetting("Mode", 0, modes));
+    this.registerSetting(speed = new SliderSetting("Speed", 45.0D, 1.0D, 100.0D, 1.0D));
+    this.registerSetting(fov = new SliderSetting("FOV", 90.0D, 15.0D, 180.0D, 1.0D));
+    this.registerSetting(distance = new SliderSetting("Distance", 4.5D, 1.0D, 10.0D, 0.5D));
+    this.registerSetting(clickAim = new ButtonSetting("Click aim", true));
+    this.registerSetting(weaponOnly = new ButtonSetting("Weapon only", false));
+    this.registerSetting(aimInvis = new ButtonSetting("Aim invis", false));
+    this.registerSetting(blatantMode = new ButtonSetting("Blatant mode", false));
+    this.registerSetting(ignoreTeammates = new ButtonSetting("Ignore teammates", false));
+    this.registerSetting(rangeFirst = new ButtonSetting("Range First", false));
+    this.registerSetting(fovPriority = new ButtonSetting("Fov Priority", false));
+    this.registerSetting(aimNecessary = new ButtonSetting("Aim Necessary", false));
+    this.registerSetting(hitThroughBlock = new ButtonSetting("Hit Through Block", false));
+  }
+
+  public void onDisable() {
+    targetEntity = null;
+  }
+
+  public void onUpdate() {
+    if (mode.getInput() != 0) {
+      return;
     }
-
-    public void onUpdate() {
-        if (mode.getInput() != 0) {
+    if (mc.currentScreen == null && mc.inGameHasFocus) {
+      if (!weaponOnly.isToggled() || Utils.holdingWeapon()) {
+        if (!clickAim.isToggled() || Utils.isClicking()) {
+          Entity en = this.getEnemy();
+          if (en != null) {
+            if (en instanceof EntityLivingBase) {
+              targetEntity = (EntityLivingBase) en;
+            }
+            if (Raven.debug) {
+              Utils.sendMessage(this.getName() + " &e" + en.getName());
+            }
+            if (blatantMode.isToggled()) {
+              Utils.aim(en, 0.0F, false);
+            } else if (speed.getInput() == 100) {
+              Utils.aim(en, 0.0F, false);
+            } else if (mode.getInput() == 0) {
+              double n = Utils.n(en);
+              if (n > 1.0D || n < -1.0D) {
+                float val = (float) (-(n / (100.0D - (speed.getInput()))));
+                mc.thePlayer.rotationYaw += val;
+              }
+            }
             return;
-        }
-        if (mc.currentScreen == null && mc.inGameHasFocus) {
-            if (!weaponOnly.isToggled() || Utils.holdingWeapon()) {
-                if (!clickAim.isToggled() || Utils.isClicking()) {
-                    Entity en = this.getEnemy();
-                    if (en != null) {
-                        if (en instanceof EntityLivingBase) {
-                            targetEntity = (EntityLivingBase) en;
-                        }
-                        if (Raven.debug) {
-                            Utils.sendMessage(this.getName() + " &e" + en.getName());
-                        }
-                        if (blatantMode.isToggled()) {
-                            Utils.aim(en, 0.0F, false);
-                        } else if (speed.getInput() == 100) {
-                            Utils.aim(en, 0.0F, false);
-                        } else if (mode.getInput() == 0) {
-                            double n = Utils.n(en);
-                            if (n > 1.0D || n < -1.0D) {
-                                float val = (float) (-(n / (100.0D - (speed.getInput()))));
-                                mc.thePlayer.rotationYaw += val;
-                            }
-                        }
-                        return;
-                    }
-                    if (shouldRender) {
-                        return;
-                    }
-                }
-            }
-        }
-        targetEntity = null;
-    }
-
-    @SubscribeEvent
-    public void onRotated(RotationEvent event) {
-        lookAt = null;
-        if (mode.getInput() != 1) {
+          }
+          if (shouldRender) {
             return;
+          }
         }
+      }
+    }
+    targetEntity = null;
+  }
 
-        if (mc.currentScreen == null && mc.inGameHasFocus) {
-            if (!weaponOnly.isToggled() || Utils.holdingWeapon()) {
-                if (!clickAim.isToggled() || Utils.isClicking()) {
-                    Entity entity = this.getEnemy();
-                    if (entity != null) {
-                        if (entity instanceof EntityLivingBase) {
-                            targetEntity = (EntityLivingBase) entity;
-                        }
-                        if (speed.getInput() == 100) {
-                            float[] rotations = RotationUtils.getRotations(entity);
-                            if (rotations != null) {
-                                float yaw = rotations[0];
-                                float pitch = MathHelper.clamp_float(rotations[1] + 4.0F, -90, 90);
-                                event.setPitch(pitch);
-                                event.setYaw(yaw);
-                                lookAt = new Float[]{yaw, pitch};
-                            }
-
-                        } else {
-                            double n = Utils.aimDiff(entity, true);
-                            float val = (float) (-(n / (100.0D - (speed.getInput()))));
-                            event.setYaw(RotationUtils.serverRotations[0] + val);
-                            lookAt = new Float[]{RotationUtils.serverRotations[0] + val};
-                        }
-                        return;
-                    }
-                    if (shouldRender) {
-                        return;
-                    }
-                }
-            }
-        }
-        targetEntity = null;
+  @SubscribeEvent
+  public void onRotated(RotationEvent event) {
+    lookAt = null;
+    if (mode.getInput() != 1) {
+      return;
     }
 
-    @SubscribeEvent
-    public void onClientLook(ClientLookEvent event) {
-        if (lookAt != null) {
-            if (lookAt.length == 2) {
-                if (lookAt[1] != null) {
-                    event.pitch = lookAt[1];
-                }
+    if (mc.currentScreen == null && mc.inGameHasFocus) {
+      if (!weaponOnly.isToggled() || Utils.holdingWeapon()) {
+        if (!clickAim.isToggled() || Utils.isClicking()) {
+          Entity entity = this.getEnemy();
+          if (entity != null) {
+            if (entity instanceof EntityLivingBase) {
+              targetEntity = (EntityLivingBase) entity;
             }
-            if (lookAt[0] != null) {
-                event.yaw = lookAt[0];
+            if (speed.getInput() == 100) {
+              float[] rotations = RotationUtils.getRotations(entity);
+              if (rotations != null) {
+                float yaw = rotations[0];
+                float pitch = MathHelper.clamp_float(rotations[1] + 4.0F, -90, 90);
+                event.setPitch(pitch);
+                event.setYaw(yaw);
+                lookAt = new Float[] {yaw, pitch};
+              }
+
+            } else {
+              double n = Utils.aimDiff(entity, true);
+              float val = (float) (-(n / (100.0D - (speed.getInput()))));
+              event.setYaw(RotationUtils.serverRotations[0] + val);
+              lookAt = new Float[] {RotationUtils.serverRotations[0] + val};
             }
+            return;
+          }
+          if (shouldRender) {
+            return;
+          }
         }
+      }
     }
+    targetEntity = null;
+  }
 
-    private Entity getEnemy() {
-        final int n = (int) fov.getInput();
-        ArrayList<Entity> players = new ArrayList<>();
-        for (final EntityPlayer entityPlayer : mc.theWorld.playerEntities) {
-            if (entityPlayer != mc.thePlayer && entityPlayer.deathTime == 0) {
-                if (Utils.isFriended(entityPlayer)) {
-                    continue;
-                }
-                if (ignoreTeammates.isToggled() && Utils.isTeamMate(entityPlayer)) {
-                    continue;
-                }
-                if (!aimInvis.isToggled() && entityPlayer.isInvisible()) {
-                    continue;
-                }
-                if (getDistanceToBoundingBox(entityPlayer) > distance.getInput()) {
-                    continue;
-                }
-                if (AntiBot.isBot(entityPlayer)) {
-                    continue;
-                }
-                if (!blatantMode.isToggled() && n != 360 && !Utils.inFov((float) n, entityPlayer)) {
-                    continue;
-                }
-                if (aimNecessary.isToggled() && mc.objectMouseOver.entityHit instanceof EntityPlayer) {
-                    targetEntity = (EntityLivingBase) mc.objectMouseOver.entityHit;
-                    shouldRender = true;
-                    return null;
-                }
-                players.add(entityPlayer);
-            }
+  @SubscribeEvent
+  public void onClientLook(ClientLookEvent event) {
+    if (lookAt != null) {
+      if (lookAt.length == 2) {
+        if (lookAt[1] != null) {
+          event.pitch = lookAt[1];
         }
-        if (!players.isEmpty()) {
-
-            if (!(fovPriority.isToggled() || rangeFirst.isToggled())) {
-                return players.get(0);
-            }
-
-            if (fovPriority.isToggled()) {
-                Entity selectEntity = players.stream().min(Comparator.comparingDouble(Utils::fovOffset)).get();
-                return selectEntity;
-            }
-
-            if (rangeFirst.isToggled()) {
-                Entity selectEntity = players.stream().min(Comparator.comparingDouble(entity -> mc.thePlayer.getDistanceToEntity(entity))).get();
-                return selectEntity;
-            }
-        }
-        shouldRender = false;
-        return null;
+      }
+      if (lookAt[0] != null) {
+        event.yaw = lookAt[0];
+      }
     }
+  }
 
-
-    private double getDistanceToBoundingBox(Entity target) {
-        if (mc.thePlayer == null) {
-            return 0;
+  private Entity getEnemy() {
+    final int n = (int) fov.getInput();
+    ArrayList<Entity> players = new ArrayList<>();
+    for (final EntityPlayer entityPlayer : mc.theWorld.playerEntities) {
+      if (entityPlayer != mc.thePlayer && entityPlayer.deathTime == 0) {
+        if (Utils.isFriended(entityPlayer)) {
+          continue;
         }
-        Vec3 playerEyePos = mc.thePlayer.getPositionEyes(((IAccessorMinecraft) mc).getTimer().renderPartialTicks);
-        AxisAlignedBB boundingBox = target.getEntityBoundingBox();
-        double nearestX = MathHelper.clamp_double(playerEyePos.xCoord, boundingBox.minX, boundingBox.maxX);
-        double nearestY = MathHelper.clamp_double(playerEyePos.yCoord, boundingBox.minY, boundingBox.maxY);
-        double nearestZ = MathHelper.clamp_double(playerEyePos.zCoord, boundingBox.minZ, boundingBox.maxZ);
-        Vec3 nearestPoint = new Vec3(nearestX, nearestY, nearestZ);
-        return playerEyePos.distanceTo(nearestPoint);
+        if (ignoreTeammates.isToggled() && Utils.isTeamMate(entityPlayer)) {
+          continue;
+        }
+        if (!aimInvis.isToggled() && entityPlayer.isInvisible()) {
+          continue;
+        }
+        if (getDistanceToBoundingBox(entityPlayer) > distance.getInput()) {
+          continue;
+        }
+        if (AntiBot.isBot(entityPlayer)) {
+          continue;
+        }
+        if (!blatantMode.isToggled() && n != 360 && !Utils.inFov((float) n, entityPlayer)) {
+          continue;
+        }
+        if (aimNecessary.isToggled() && mc.objectMouseOver.entityHit instanceof EntityPlayer) {
+          targetEntity = (EntityLivingBase) mc.objectMouseOver.entityHit;
+          shouldRender = true;
+          return null;
+        }
+        players.add(entityPlayer);
+      }
     }
+    if (!players.isEmpty()) {
+
+      if (!(fovPriority.isToggled() || rangeFirst.isToggled())) {
+        return players.get(0);
+      }
+
+      if (fovPriority.isToggled()) {
+        Entity selectEntity =
+            players.stream().min(Comparator.comparingDouble(Utils::fovOffset)).get();
+        return selectEntity;
+      }
+
+      if (rangeFirst.isToggled()) {
+        Entity selectEntity =
+            players.stream()
+                .min(Comparator.comparingDouble(entity -> mc.thePlayer.getDistanceToEntity(entity)))
+                .get();
+        return selectEntity;
+      }
+    }
+    shouldRender = false;
+    return null;
+  }
+
+  private double getDistanceToBoundingBox(Entity target) {
+    if (mc.thePlayer == null) {
+      return 0;
+    }
+    Vec3 playerEyePos =
+        mc.thePlayer.getPositionEyes(((IAccessorMinecraft) mc).getTimer().renderPartialTicks);
+    AxisAlignedBB boundingBox = target.getEntityBoundingBox();
+    double nearestX =
+        MathHelper.clamp_double(playerEyePos.xCoord, boundingBox.minX, boundingBox.maxX);
+    double nearestY =
+        MathHelper.clamp_double(playerEyePos.yCoord, boundingBox.minY, boundingBox.maxY);
+    double nearestZ =
+        MathHelper.clamp_double(playerEyePos.zCoord, boundingBox.minZ, boundingBox.maxZ);
+    Vec3 nearestPoint = new Vec3(nearestX, nearestY, nearestZ);
+    return playerEyePos.distanceTo(nearestPoint);
+  }
 }
