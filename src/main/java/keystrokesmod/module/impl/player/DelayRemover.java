4e6ceea681d4dc6ae1452925c2bc6bf3a8222c37
sasiloxr <sasiloxr2@gmail.com>
2025-07-07 20:38:33 +0800

    start replace

--- src/main/java/keystrokesmod/module/impl/player/DelayRemover.java
+++ src/main/java/keystrokesmod/module/impl/player/DelayRemover.java
@@ -10,32 +10,37 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class DelayRemover extends Module {
-    public static ButtonSetting oldReg, removeJumpTicks;
-    public static SliderSetting jumpDelay;
-    public static int countTicks = 0;
+  public static ButtonSetting oldReg, removeJumpTicks;
+  public static SliderSetting jumpDelay;
+  public static ButtonSetting disableInWater;
+  public static int countTicks = 0;
 
-    public DelayRemover() {
-        super("Delay Remover", category.player, 0);
-        this.registerSetting(oldReg = new ButtonSetting("1.7 hitreg", true));
-        this.registerSetting(removeJumpTicks = new ButtonSetting("Remove jump ticks", false));
-        this.registerSetting(jumpDelay = new SliderSetting("Jump Delay", 0, 0, 10, 1));
-        this.closetModule = true;
-    }
+  public DelayRemover() {
+    super("Delay Remover", category.player, 0);
+    this.registerSetting(oldReg = new ButtonSetting("1.7 hitreg", true));
+    this.registerSetting(removeJumpTicks = new ButtonSetting("Remove jump ticks", false));
+    this.registerSetting(jumpDelay = new SliderSetting("Jump Delay", 0, 0, 10, 1));
+    this.registerSetting(disableInWater = new ButtonSetting("Disable in water", true));
+    this.closetModule = true;
+  }
 
-    @SubscribeEvent
-    public void onTick(final TickEvent.PlayerTickEvent event) {
-        if (event.phase != TickEvent.Phase.END || !mc.inGameHasFocus || !Utils.nullCheck()) {
-            return;
-        }
-        if (oldReg.isToggled()) {
-            ((IAccessorMinecraft) mc).setLeftClickCounter(0);
-        }
-        if (removeJumpTicks.isToggled()) {
-            int jumpTick = (int) jumpDelay.getInput();
-            int currentJumpTick = ((IAccessorEntityLivingBase) mc.thePlayer).getJumpTicks();
-            if (currentJumpTick > jumpTick) {
-                ((IAccessorEntityLivingBase) mc.thePlayer).setJumpTicks(jumpTick);
-            }
-        }
+  @SubscribeEvent
+  public void onTick(final TickEvent.PlayerTickEvent event) {
+    if (event.phase != TickEvent.Phase.END || !mc.inGameHasFocus || !Utils.nullCheck()) {
+      return;
     }
+    if (oldReg.isToggled()) {
+      ((IAccessorMinecraft) mc).setLeftClickCounter(0);
+    }
+    if (disableInWater.isToggled() && mc.thePlayer.isInWater()) {
+      return;
+    }
+    if (removeJumpTicks.isToggled()) {
+      int jumpTick = (int) jumpDelay.getInput();
+      int currentJumpTick = ((IAccessorEntityLivingBase) mc.thePlayer).getJumpTicks();
+      if (currentJumpTick > jumpTick) {
+        ((IAccessorEntityLivingBase) mc.thePlayer).setJumpTicks(jumpTick);
+      }
+    }
+  }
 }
